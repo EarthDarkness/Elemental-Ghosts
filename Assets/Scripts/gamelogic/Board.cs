3efce534b0ad0e193eb0133eb09160084917136b
EarthDarkness <demetrius_psf@hotmail.com>
2018-01-27 03:58:16 -0200

    Added Broad class that handles creation of tilemaps.

---
+++ Assets/Scripts/gamelogic/Board.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileCell : MonoBehaviour{
+	public List<GameObject> _component = new List<GameObject>();
+	GameObject _element = null;
+
+	/*void Awake() {
+		for(int i=0;i<transform.childCount;++i) {
+			_component.Add(transform.GetChild(i).gameObject);
+		}
+	}*/
+
+	void OnValidate() {
+		for(int i=transform.childCount;i<_component.Count;++i) {
+			//if(_component[i] == null) {
+			//	_component.RemoveAt(i);
+			//}else {
+				Instantiate(_component[i],transform);
+			//}
+		}
+		/*for(int i=0;i<_component.Count;++i) {
+			if (transform.childCount > i || _component[i] == null)
+				continue;
+			_
+		}*/
+	}
+
+	public TileCell() {
+
+	}
+
+
+
+};
+
+public class Board : MonoBehaviour{
+
+
+    public int _width   = 20;
+    public int _height  = 20;
+
+	GameObject[,] _map;
+
+	[EasyButtons.Button()]
+	void Deploy() {
+		_map = new GameObject[_width,_height];
+		
+		for(int j=0;j<_height;++j) {
+			GameObject line = new GameObject();
+			line.name = "Row "+j.ToString();
+			line.transform.parent = transform;
+			for(int i=0;i<_width;++i) {
+				_map[i,j] = new GameObject();
+				_map[i,j].transform.parent = line.transform;
+				_map[i,j].AddComponent<TileCell>();
+				_map[i, j].name = "Col "+i.ToString();
+			}
+		}
+
+	}
+	[EasyButtons.Button()]
+	void Reset() {
+		if (_map == null)
+			return;
+		for(int j=0;j<_height;++j) {
+			for(int i=0;i<_width;++i) {
+				//if (_map[i, j] == null)
+				//	continue;
+				_map[i,j].GetComponent<TileCell>()._component.Clear();
+				DestroyImmediate(_map[i,j]);
+				_map[i,j] = null;
+			}
+		}
+		/*for(int i=transform.childCount-1;i>=0;++i) {
+			DestroyImmediate(transform.GetChild(i).gameObject);
+		}*/
+		while(transform.childCount > 0) {
+			DestroyImmediate(transform.GetChild(transform.childCount-1).gameObject);
+		}
+
+	}
+
+
+}
+
+
+
