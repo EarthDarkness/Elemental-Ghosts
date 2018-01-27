01f03bd1d57a32eecd98e00b4453a197724542f9
EarthDarkness <demetrius_psf@hotmail.com>
2018-01-27 09:41:51 -0200

    Fixed issue with TileCell added tilemap to testscene again.

---
+++ Assets/Scripts/gamelogic/TileCell.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TileCell : MonoBehaviour{
+	public bool _lock = false;
+	public List<GameObject> _component = new List<GameObject>();
+	GameObject _elementVisual = null;
+	public int _elementType = 6;//6 = no element (TODO element type enum)
+
+
+	/*void Awake() {
+		for(int i=0;i<transform.childCount;++i) {
+			_component.Add(transform.GetChild(i).gameObject);
+		}
+	}*/
+
+	void OnValidate() {
+		if (_lock)
+			return;
+		for(int i=transform.childCount;i<_component.Count;++i) {
+			//if(_component[i] == null) {
+			//	_component.RemoveAt(i);
+			//}else {
+				Instantiate(_component[i],transform);
+			//}
+		}
+		/*for(int i=0;i<_component.Count;++i) {
+			if (transform.childCount > i || _component[i] == null)
+				continue;
+			_
+		}*/
+	}
+
+	public TileCell() {
+
+	}
+
+
+
+};
