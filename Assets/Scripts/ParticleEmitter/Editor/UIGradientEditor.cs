dadf76f8557927737f010be98d63d432bf171863
DESKTOP-6S01MSG\Rodrigo <rodrigo@napalm.studio>
2018-01-27 15:27:42 -0200

    MainScene

---
+++ Assets/Scripts/ParticleEmitter/Editor/UIGradientEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(UIGradient))]
+public class UIGradientEditor : Editor {
+
+	public override void OnInspectorGUI()
+	{
+		UIGradient script = (UIGradient)target;
+
+		script.colors[0] = EditorGUILayout.ColorField ("Bot1", script.colors[0]);
+		script.colors[3] = EditorGUILayout.ColorField ("Bot2", script.colors[3]);
+		script.colors[1] = EditorGUILayout.ColorField ("Top1", script.colors[1]);
+		script.colors[2] = EditorGUILayout.ColorField ("Top2", script.colors[2]);
+		
+	}
+	void OnSceneGUI(){
+
+		
+		
+
+	}
+}
