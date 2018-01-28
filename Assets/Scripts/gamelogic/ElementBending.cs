e9681583702cf4076823db663d4da282ce55e6a8
Luiz Augusto Wendt <luizaugustow_1@msn.com>
2018-01-28 15:04:58 -0200

    fixed canvas scalling

--- Assets/Scripts/gamelogic/ElementBending.cs
+++ Assets/Scripts/gamelogic/ElementBending.cs
@@ -89,7 +89,7 @@ public class ElementBending : MonoBehaviour
     public void ResetStates()
     {
         ElementType = EType.Neutral;
-        DisabpleAura();
+        DisableAura();
     }
 
     public void ChangeModel()
