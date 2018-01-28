52c1f8d551c41f4eff5bee0bea97317666b15ac8
usuario <usuario@1213535-U.asav.brm>
2018-01-28 10:42:06 -0200

    Auras

--- Assets/Scripts/gamelogic/ElementBending.cs
+++ Assets/Scripts/gamelogic/ElementBending.cs
@@ -12,11 +12,10 @@ public class ElementBending : MonoBehaviour
     public bool buffed = false;
     public List<ElementModel> listElement;
 
-    [InspectorReadOnly]
-    private GameObject[] playerModels = new GameObject[6];
+    public List<ElementModel> auraList;
 
-	public static float pickupbasetimer = 0.5f;  
-	public float elementalPickup = 0.0f;  
+    public static float pickupbasetimer = 0.5f;
+    public float elementalPickup = 0.0f;
 
 
     public float timeDeath = 0.2f;
@@ -52,24 +51,18 @@ public class ElementBending : MonoBehaviour
         playerData = GetComponent<PlayerData>();
         ElementType = EType.Neutral;
 
-
-        for (int i = 0; i < gameObject.transform.GetChild(0).childCount; i++)
-        {
-            playerModels[i] = gameObject.transform.GetChild(0).GetChild(i).gameObject;
-        }
-
     }
-	void Update() {  
-		if (elementalPickup > 0.0f)  
-			elementalPickup -= Time.deltaTime;  
-	}  
+    void Update() {
+        if (elementalPickup > 0.0f)
+            elementalPickup -= Time.deltaTime;
+    }
 
     public void Action()
     {
         if (currentType == EType.Neutral)
         {
             //Signals.Get<PickupElement>().Dispatch(this);
-			elementalPickup = pickupbasetimer; 
+            elementalPickup = pickupbasetimer;
         }
         else
         {
@@ -82,15 +75,19 @@ public class ElementBending : MonoBehaviour
         yield return new WaitForSeconds(castingTime);
         GameObject newProjectile = Instantiate(projectile, transform.position, Quaternion.LookRotation(transform.forward, transform.up));
         newProjectile.GetComponent<Projectile>().Initialize(playerData, transform.forward, currentType, this.buffed);
-        ElementType =EType.Neutral;
+        ElementType = EType.Neutral;
         Physics.IgnoreCollision(GetComponent<Collider>(), newProjectile.GetComponent<Collider>());
-
     }
 
     public void ChangeModel()
     {
         ElementModel.ChangeModel(listElement, currentType);
 
+    }
+
+    public void ChangeAura()
+    {
+        ElementModel.ChangeModel(auraList, currentType);
     }
 
 
