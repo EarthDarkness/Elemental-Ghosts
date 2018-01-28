346c57596712fa6ec40edae1fb65309e0c80a1f3
Geese Howard <geeseh@southtown.com>
2018-01-28 15:09:19 -0200

    Arrowsgit add .git add .git add .!

---
+++ Assets/LookAtCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour {
+    
+	void Update () {
+        transform.LookAt(Camera.main.transform);
+        transform.eulerAngles = new Vector3(0f, transform.eulerAngles.y, 0f);
+
+    }
+}
