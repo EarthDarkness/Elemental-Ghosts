87ed35f9c399756f01d65dc835452640f5e87174
Luiz Augusto Wendt <luizaugustow_1@msn.com>
2018-01-28 13:05:24 -0200

    Musics

--- Assets/SceneLoader.cs
+++ Assets/SceneLoader.cs
@@ -17,8 +17,8 @@ public class SceneLoader : MonoBehaviour {
 	public void LoadScene(int scene)
 	{
 		SceneManager.LoadScene (scene);
-	}
-	public void Quit(){
+    }
+    public void Quit(){
 		Application.Quit ();
 	}
 }
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        AudioManager.Instance.PlaySound("Menu", 1);
+    }
+
+
+}
